6b7be14650dfa48a15c81922074ae87b6f04790f
Tom Humphreys <Tom01098Code@gmail.com>
2019-03-09 21:11:41 +0000

    Initial structure

---
+++ Jelly/Jelly.Benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Jelly.Benchmarks
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Jelly/Jelly/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Jelly
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
