2f68b7064a2ff57bd40ed547d78d80108a933eb4
Tom Humphreys <Tom01098Code@gmail.com>
2019-03-10 18:08:51 +0000

    Identifiers, keywords and numbers

--- Jelly/Jelly.Core/Parsing/Lexer.cs
+++ Jelly/Jelly.Core/Parsing/Lexer.cs
@@ -1,6 +1,8 @@
 ﻿using Jelly.Core.Parsing.Tokens;
 using System;
 using System.Collections.Generic;
+using Jelly.Core.Utility;
+using static System.Char;
 
 namespace Jelly.Core.Parsing
 {
@@ -16,9 +18,84 @@ namespace Jelly.Core.Parsing
 
             var tokens = new List<Token>();
 
+            while (index < span.Length)
+            {
+                // Identifiers/Keywords
+                if (IsLetter(span[index]))
+                {
+                    var start = index;
+                    var position = GetPosition();
 
+                    do
+                    {
+                        NextChar();
+                    }
+                    while (index < span.Length && IsLetter(span[index]));
+
+                    var identifier = span.Slice(start, index - start).ToString();
+
+                    switch (identifier)
+                    {
+                        case "if":
+                            tokens.Add(new KeywordToken(KeywordType.If, position));
+                            break;
+                        case "elif":
+                            tokens.Add(new KeywordToken(KeywordType.Elif, position));
+                            break;
+                        case "else":
+                            tokens.Add(new KeywordToken(KeywordType.Else, position));
+                            break;
+                        case "end":
+                            tokens.Add(new KeywordToken(KeywordType.End, position));
+                            break;
+                        default:
+                            tokens.Add(new IdentifierToken(identifier, position));
+                            break;
+                    }
+
+                    continue;
+                }
+                // Numbers
+                else if (IsDigit(span[index]))
+                {
+                    var start = index;
+                    var position = GetPosition();
+
+                    do
+                    {
+                        NextChar();
+                    }
+                    while (index < span.Length && (IsDigit(span[index]) || span[index] == '.'));
+
+                    var number = double.Parse(span.Slice(start, index - start));
+
+                    tokens.Add(new NumberToken(number, position));
+
+                    continue;
+                }
+
+                NextChar();
+            }
+
+            tokens.Add(new EOFToken(GetPosition()));
 
             return tokens;
+
+            // Local functions
+            Position GetPosition() => new Position(lineNumber, characterNumber);
+
+            void NextChar()
+            {
+                index++;
+                characterNumber++;
+            }
+
+            void NextLine()
+            {
+                index++;
+                lineNumber++;
+                characterNumber = 1;
+            }
         }
     }
 }
