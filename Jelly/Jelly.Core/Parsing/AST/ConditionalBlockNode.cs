5bdd4fc3be32a427acb460d0fe2475aaf35de666
Tom Humphreys <Tom01098Code@gmail.com>
2019-03-15 14:23:06 +0000

    Fixed issue with the else syntax and finished off the AST nodes.

---
+++ Jelly/Jelly.Core/Parsing/AST/ConditionalBlockNode.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Jelly.Core.Parsing.AST
+{
+    public class ConditionalBlockNode
+    {
+        public IValueNode Condition { get; }
+        public List<IConstructNode> Constructs { get; }
+
+        public ConditionalBlockNode(IValueNode condition,
+                                    List<IConstructNode> constructs) =>
+            (Condition, Constructs) = (condition, constructs);
+    }
+}
---
+++ Jelly/Jelly.Core/Parsing/AST/IfBlockNode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Jelly.Core.Parsing.AST
+{
+    public class IfBlockNode : IConstructNode
+    {
+        public List<ConditionalBlockNode> ConditionalBlocks { get; }
+
+        public IfBlockNode(List<ConditionalBlockNode> conditionalBlocks) =>
+            ConditionalBlocks = conditionalBlocks;
+    }
+}
