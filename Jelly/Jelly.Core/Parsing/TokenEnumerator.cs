efec0c81f4ecac47e0e3141178a3679ab84d3218
Tom Humphreys <Tom01098Code@gmail.com>
2019-03-16 09:08:28 +0000

    TokenEnumerator with lookahead capabilities.

---
+++ Jelly/Jelly.Core/Parsing/TokenEnumerator.cs
@@ -0,0 +1,60 @@
+﻿using Jelly.Core.Parsing.Tokens;
+using System.Collections.Generic;
+
+namespace Jelly.Core.Parsing
+{
+    public class TokenEnumerator
+    {
+        private List<Token> tokens;
+        private Stack<int> indices;
+
+        public TokenEnumerator(List<Token> tokens)
+        {
+            this.tokens = tokens;
+            indices = new Stack<int>();
+        }
+
+        public Token Current
+        {
+            get
+            {
+                if (indices.TryPeek(out var index))
+                {
+                    if (index < tokens.Count)
+                    {
+                        return tokens[index];
+                    }
+                }
+
+                return null;
+            }
+        }
+
+        public void MoveNext()
+        {
+            if (indices.TryPop(out var index))
+            {
+                indices.Push(index + 1);
+            }
+            else
+            {
+                indices.Push(0);
+            }
+        }
+
+        public Token LookAhead(int num)
+        {
+            if (indices.TryPeek(out var index))
+            {
+                var newIndex = index + num;
+
+                if (newIndex < tokens.Count)
+                {
+                    return tokens[newIndex];
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Jelly/Jelly.Tests/TokenEnumeratorTests.cs
@@ -0,0 +1,75 @@
+﻿using Jelly.Core.Parsing;
+using Jelly.Core.Parsing.Tokens;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using static Jelly.Tests.TestUtility;
+
+namespace Jelly.Tests
+{
+    [TestClass]
+    public class TokenEnumeratorTests
+    {
+        [TestMethod]
+        public void Enumerate()
+        {
+            var tokens = new List<Token>
+            {
+                new IdentifierToken("x", Position(1, 1)),
+                new SymbolToken(SymbolType.Assignment, Position(1, 2)),
+                new NumberToken(4.2, Position(1, 3)),
+                new EOLToken(Position(1, 6)),
+                new EOFToken(Position(1, 6))
+            };
+
+            var enumerator = new TokenEnumerator(tokens);
+
+            Assert.AreEqual(null, enumerator.Current);
+            enumerator.MoveNext();
+            Assert.AreEqual(tokens[0], enumerator.Current);
+            enumerator.MoveNext();
+            Assert.AreEqual(tokens[1], enumerator.Current);
+            enumerator.MoveNext();
+            Assert.AreEqual(tokens[2], enumerator.Current);
+            enumerator.MoveNext();
+            Assert.AreEqual(tokens[3], enumerator.Current);
+            enumerator.MoveNext();
+            Assert.AreEqual(tokens[4], enumerator.Current);
+            enumerator.MoveNext();
+            Assert.AreEqual(null, enumerator.Current);
+            enumerator.MoveNext();
+        }
+
+        [TestMethod]
+        public void EnumerateWithLookAhead()
+        {
+            var tokens = new List<Token>
+            {
+                new IdentifierToken("x", Position(1, 1)),
+                new SymbolToken(SymbolType.Assignment, Position(1, 2)),
+                new NumberToken(4.2, Position(1, 3)),
+                new EOLToken(Position(1, 6)),
+                new EOFToken(Position(1, 6))
+            };
+
+            var enumerator = new TokenEnumerator(tokens);
+
+            Assert.AreEqual(null, enumerator.Current);
+            enumerator.MoveNext();
+            Assert.AreEqual(tokens[0], enumerator.Current);
+            enumerator.MoveNext();
+            Assert.AreEqual(tokens[3], enumerator.LookAhead(2));
+            Assert.AreEqual(tokens[1], enumerator.Current);
+            enumerator.MoveNext();
+            Assert.AreEqual(tokens[2], enumerator.Current);
+            enumerator.MoveNext();
+            Assert.AreEqual(tokens[3], enumerator.Current);
+            Assert.AreEqual(tokens[4], enumerator.LookAhead(1));
+            Assert.AreEqual(null, enumerator.LookAhead(2));
+            enumerator.MoveNext();
+            Assert.AreEqual(tokens[4], enumerator.Current);
+            enumerator.MoveNext();
+            Assert.AreEqual(null, enumerator.Current);
+            enumerator.MoveNext();
+        }
+    }
+}
