88ce3ca00fa8a73fa6b59b87fdcfeb4ea3a6f9f3
Tom Humphreys <Tom01098Code@gmail.com>
2019-03-17 16:41:53 +0000

    Console environment for lexing and parsing a file. Fun fact - parser can't handle the Factorial example, but it can do the other two.

--- Jelly/Jelly/Program.cs
+++ Jelly/Jelly/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using Jelly.Core.Parsing;
+using Jelly.Core.Utility;
 
 namespace Jelly
 {
@@ -6,7 +9,24 @@ namespace Jelly
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine($"Begin reading from {args[0]}");
+                var text = File.ReadAllText(args[0]);
+                Console.WriteLine("Begin lexing");
+                var tokens = new Lexer().Lex(text, args[0]);
+                Console.WriteLine("Begin parsing");
+                var ast = new Parser().Parse(tokens);
+                Console.WriteLine("Success");
+            }
+            catch (JellyException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unhandled error: {e}");
+            }
 
             Console.ReadKey();
         }
