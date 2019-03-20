de033a5387e66db734e93595c5f2578e2b8e630f
Tom Humphreys <Tom01098Code@gmail.com>
2019-03-20 15:50:26 +0000

    Created a really basic interpreter stub.

---
+++ Jelly/Jelly.Core/Interpreting/Interpreter.cs
@@ -0,0 +1,28 @@
+﻿using Jelly.Core.Parsing.AST;
+using System.Collections.Generic;
+
+namespace Jelly.Core.Interpreting
+{
+    /// <summary>
+    /// Interpret a given AST at run time
+    /// </summary>
+    public class Interpreter
+    {
+        private Dictionary<string, FunctionNode> functions;
+
+        public void Interpret(List<FunctionNode> ast)
+        {
+            CreateDictionary(ast);
+        }
+
+        private void CreateDictionary(List<FunctionNode> ast)
+        {
+            functions = new Dictionary<string, FunctionNode>();
+
+            foreach (var node in ast)
+            {
+                functions.Add(node.Identifier.Identifier, node);
+            }
+        }
+    }
+}
