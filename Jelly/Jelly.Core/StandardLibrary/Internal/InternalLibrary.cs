6d00f71a3b28bda5cb0d665eb9e7577f7b733950
Tom Humphreys <Tom01098Code@gmail.com>
2019-03-22 14:22:07 +0000

    Moved the internal function attribute, added IsNaN and WriteChar.

--- Jelly/Jelly.Core/StandardLibrary/Internal/InternalLibrary.cs
+++ Jelly/Jelly.Core/StandardLibrary/Internal/InternalLibrary.cs
@@ -20,6 +20,20 @@ namespace Jelly.Core.StandardLibrary.Internal
         }
 
         [InternalFunction]
+        public static double WriteNewLine()
+        {
+            Console.WriteLine();
+            return double.NaN;
+        }
+
+        [InternalFunction]
+        public static double WriteChar(double value)
+        {
+            Console.Write(char.ConvertFromUtf32((int)value));
+            return double.NaN;
+        }
+
+        [InternalFunction]
         public static double Read()
         {
             return double.Parse(Console.ReadLine());
@@ -29,6 +43,18 @@ namespace Jelly.Core.StandardLibrary.Internal
         public static double Pow(double a, double b)
         {
             return Math.Pow(a, b);
+        }
+
+        [InternalFunction]
+        public static double Sqrt(double x)
+        {
+            return Math.Sqrt(x);
+        }
+
+        [InternalFunction]
+        public static double IsNaN(double x)
+        {
+            return double.IsNaN(x) ? 1 : 0;
         }
     }
 }
