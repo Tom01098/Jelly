230a59b2f4b71a31a085ceb4d731efcc2dbb52a9
Tom Humphreys <Tom01098Code@gmail.com>
2019-03-28 10:12:37 +0000

    RNG in the library

--- Jelly/Jelly.Core/StandardLibrary/Internal/InternalLibrary.cs
+++ Jelly/Jelly.Core/StandardLibrary/Internal/InternalLibrary.cs
@@ -4,6 +4,7 @@ namespace Jelly.Core.StandardLibrary.Internal
 {
     public class InternalLibrary
     {
+        #region IO
         [InternalFunction]
         public static double Write(double value)
         {
@@ -36,11 +37,34 @@ namespace Jelly.Core.StandardLibrary.Internal
         {
             return Math.Pow(a, b);
         }
+        #endregion
 
+        #region Number Utility
         [InternalFunction]
         public static double IsNaN(double x)
         {
             return double.IsNaN(x) ? 1 : 0;
         }
+        #endregion
+
+        #region Random
+        [InternalFunction]
+        public static double RandomInt()
+        {
+            return new Random().Next();
+        }
+
+        [InternalFunction]
+        public static double RandomIntBetween(double min, double max)
+        {
+            return new Random().Next((int)min, (int)max);
+        }
+
+        [InternalFunction]
+        public static double Random()
+        {
+            return new Random().NextDouble();
+        }
+        #endregion
     }
 }
